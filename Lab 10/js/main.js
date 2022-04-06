3fb5effce500d6b901b080da39ff3839928e8cf2
ggmaurer <ggmaurer@wisc.edu>
2022-04-06 16:20:08 -0500

    Started Lab 10

--- Lab 10/js/main.js
+++ Lab 10/js/main.js
@@ -28,6 +28,8 @@ function setMap(){
                     d3.json("data/usStates.topojson"),   
                     d3.json("data/surroundingCountries.topojson")                
                     ];    
+
+
     Promise.all(promises).then(callback);
 
     function callback(data){
@@ -36,8 +38,38 @@ function setMap(){
             states = data[1],
             country = data [2];
 
+        console.log(csvData);
+
         var usStates = topojson.feature(states, states.objects.ne_50m_admin_1_states_provinces).features,
             surroundingCountries = topojson.feature(country, country.objects.ne_50m_admin_0_countries);
+
+        //variables for data join
+        var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+
+        //loop through csv to assign each set of csv attribute values to geojson region
+        for (var i=0; i<csvData.length; i++){
+            var csvRegion = csvData[i]; //the current region
+            var csvKey = csvRegion.adm1_code; //the CSV primary key
+
+            //loop through geojson regions to find correct region
+            for (var a=0; a<usStates.length; a++){
+
+                var geojsonProps = usStates[a].properties; //the current region geojson properties
+                var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+                //where primary keys match, transfer csv data to geojson properties object
+                if (geojsonKey == csvKey){
+
+                    //assign all attributes and values
+                    attrArray.forEach(function(attr){
+                        var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                        geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                    });
+                };
+            };
+        };
+
+        console.log(attrArray);
 
             var graticule = d3.geoGraticule().step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
